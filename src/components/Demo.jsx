bb4399ed9c7d7f697b5ef2b272fe01bd63772f5a
SainiAditya1 <Adityasaini10012001@outlook.com>
2023-04-23 20:17:25 +0530

    added api
    Signed-off-by: SainiAditya1 <Adityasaini10012001@outlook.com>

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+
 import { copy,linkIcon, loader, tick } from '../assets';
+
+import { useLazyGetSummaryQuery } from '../services/article';
 
 const Demo = () => {
   const [article, setArticle] = useState({
@@ -9,9 +12,25 @@ const Demo = () => {
 
   });
 
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+
+
+
+
   const handleSubmit = async (e) => {
     // fetch summary
-    alert('Submitted');
+    // alert('Submitted');
+    e.preventDefault();
+    const { data } = await getSummary({ articleUrl: article.url});
+
+    if(data?.summary) {
+      const newArticle = {...article, summary: data.summary};
+
+      setArticle(newArticle);
+      console.log(newArticle);
+
+    }
   }
 
   return (
